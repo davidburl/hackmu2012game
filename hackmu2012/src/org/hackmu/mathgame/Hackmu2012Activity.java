495cfe57756c5c1b5b06b57ac213b1f94b08d0af
David Burl <davidoburl@gmail.com>
2012-04-21 16:09:35 -0400

    Initial commit.

---
+++ hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
@@ -0,0 +1,13 @@
+package org.hackmu.mathgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hackmu2012Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
