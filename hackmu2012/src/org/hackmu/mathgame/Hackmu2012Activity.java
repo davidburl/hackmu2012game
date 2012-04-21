707ee29a6a5f802f1fdc5bcee88a4c395169a4ff
David Burl <davidoburl@gmail.com>
2012-04-21 19:26:47 -0400

    quick fix

--- hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
+++ hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
@@ -2,12 +2,35 @@ package org.hackmu.mathgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Hackmu2012Activity extends Activity {
+
+public class Hackmu2012Activity extends Activity implements OnClickListener {
+	
+	Button answerOne, answerTwo, answerThree, answerFour, storeOne, storeTwo;
+
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        answerOne = (Button)findViewById(R.id.answerOne);
+        answerTwo = (Button)findViewById(R.id.answerTwo);
+        answerThree = (Button)findViewById(R.id.answerThree);
+        answerFour = (Button)findViewById(R.id.answerFour);
+        
+        
+        answerOne.setOnClickListener(this);
+    }
+    
+   
+    
+    public void onClick(View src) {
+    	
     }
 }
