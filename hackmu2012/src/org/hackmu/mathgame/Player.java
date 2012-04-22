7d523daa699edecd92a86a835dd07f219809ef16
Matthew Vaughn <abstract@mabv.org>
2012-04-21 23:51:48 -0400

    Player contents added

--- hackmu2012/src/org/hackmu/mathgame/Player.java
+++ hackmu2012/src/org/hackmu/mathgame/Player.java
@@ -1,5 +1,44 @@
 package org.hackmu.mathgame;
 
 public class Player {
+	private int mPoints = 10;
+	private int mIconID = R.id.playerImage;
+	private int mCurrentHP = 100;
+	private int mMaxHP = 100;
 
+	public Player() {
+		// nothing
+	}
+
+	public int getPoints() {
+		return this.mPoints;
+	}
+	
+	public void setPoints(int newPoints) {
+		this.mPoints = newPoints;
+	}
+
+	public int getIconID() {
+		return this.mIconID;
+	}
+	
+	public void setIconID(int newIconID) {
+		this.mIconID = newIconID;
+	}
+
+	public int getCurrentHP() {
+		return this.mCurrentHP;
+	}
+
+	public void setCurrentHP(int newCurrentHP) {
+		this.mCurrentHP = newCurrentHP;
+	}
+	
+	public int getMaxHP() {
+		return this.mMaxHP;
+	}
+	 
+	public void setMaxHP(int newMaxHP) {
+		this.mMaxHP = newMaxHP;
+	}
 }
