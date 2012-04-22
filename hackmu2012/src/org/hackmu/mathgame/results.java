a3be27b1608453fb5e132802f9373c06e8d30267
David Burl <davidoburl@gmail.com>
2012-04-21 23:11:07 -0400

    *really* added the results

---
+++ hackmu2012/src/org/hackmu/mathgame/results.java
@@ -0,0 +1,33 @@
+package org.hackmu.mathgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+
+
+public class results extends Activity {
+	
+	ProgressBar overall, trig, integral;
+	
+	public void onCreate(Bundle savedInstanceState) 
+	{
+	        super.onCreate(savedInstanceState);
+			setContentView(R.layout.results);
+			
+			Bundle b = getIntent().getExtras();
+			
+			overall = (ProgressBar) findViewById(R.id.mainProgress);
+			trig = (ProgressBar) findViewById(R.id.trigProgress);
+			integral = (ProgressBar) findViewById(R.id.integralBar);
+			
+			overall.setMax(1000);
+			trig.setMax(1000);
+			integral.setMax(1000);
+			
+			overall.setProgress((int) (b.getDouble("overall")*1000));
+			trig.setProgress((int) (b.getDouble("trig")*1000));
+			integral.setProgress((int) (b.getDouble("int")*1000));
+
+	}
+}
