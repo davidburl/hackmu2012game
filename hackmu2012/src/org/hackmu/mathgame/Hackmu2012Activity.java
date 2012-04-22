c36f51b3707b4cfcddf381c7707e9fa56dc39e82
David Burl <davidoburl@gmail.com>
2012-04-22 11:31:55 -0400

    fixed problem were backing out of the application called onquit even when the app was in the background

--- hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
+++ hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
@@ -164,6 +164,11 @@ public class Hackmu2012Activity extends Activity implements OnClickListener {
 		mHandler.postDelayed(updateProgress, ONE_SECOND / 10);
 	}
 	
+	public void onPause() {
+		super.onPause();
+		mHandler.removeCallbacks(updateProgress);
+	}
+	
 	public void changePlayerHealth(int health) {
 		// update progress to current player health in case we 
 		// were in the process of a transition previously
