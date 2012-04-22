24d3fff4f592ca0c7e34a36a1fb3147062dfe8e8
Matthew Vaughn <abstract@mabv.org>
2012-04-22 08:27:26 -0400

    updated views

--- hackmu2012/gen/org/hackmu/mathgame/R.java
+++ hackmu2012/gen/org/hackmu/mathgame/R.java
@@ -12,12 +12,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int brushedmetal_oceanblue=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ntitled=0x7f020002;
-        public static final int player_david_avatar=0x7f020003;
-        public static final int trig_force=0x7f020004;
-        public static final int trogdor_integral=0x7f020005;
-        public static final int user_male=0x7f020006;
+        public static final int brushedmetal_scarletred=0x7f020001;
+        public static final int brushedmetal_silver=0x7f020002;
+        public static final int healthprogress=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ntitled=0x7f020005;
+        public static final int player_david_avatar=0x7f020006;
+        public static final int trainer_avatar=0x7f020007;
+        public static final int trig_force=0x7f020008;
+        public static final int trogdor_integral=0x7f020009;
+        public static final int user_male=0x7f02000a;
     }
     public static final class id {
         public static final int answerFour=0x7f05000e;
@@ -27,8 +31,8 @@ public final class R {
         public static final int enemyHealth=0x7f050001;
         public static final int enemyName=0x7f050000;
         public static final int imageView1=0x7f050002;
-        public static final int integralBar=0x7f05001a;
-        public static final int mainProgress=0x7f050016;
+        public static final int integralBar=0x7f050018;
+        public static final int mainProgress=0x7f05001a;
         public static final int moreHP=0x7f050013;
         public static final int moreTime=0x7f050012;
         public static final int playerHealth=0x7f050005;
@@ -39,11 +43,11 @@ public final class R {
         public static final int questionText=0x7f05000a;
         public static final int store=0x7f05000f;
         public static final int textView1=0x7f050014;
-        public static final int textView2=0x7f050015;
-        public static final int textView3=0x7f050017;
-        public static final int textView4=0x7f050019;
+        public static final int textView2=0x7f050019;
+        public static final int textView3=0x7f050015;
+        public static final int textView4=0x7f050017;
         public static final int timeLeft=0x7f050009;
-        public static final int trigProgress=0x7f050018;
+        public static final int trigProgress=0x7f050016;
         public static final int txtProg=0x7f050006;
         public static final int txtSec=0x7f050008;
         public static final int txtTimeLeft=0x7f050007;
