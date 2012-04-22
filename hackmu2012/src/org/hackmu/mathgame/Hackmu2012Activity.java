d8eaee90b639fe300b555f3b915cad25908259dd
David Burl <davidoburl@gmail.com>
2012-04-21 21:56:19 -0400

    Added easy accessor methods for HackMu

--- hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
+++ hackmu2012/src/org/hackmu/mathgame/Hackmu2012Activity.java
@@ -9,79 +9,113 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-
 // Yoshi WILL rule the world.  There is no escape...
 public class Hackmu2012Activity extends Activity implements OnClickListener {
-	
-	public final static int ONE_SECOND = 1000;	
-	
+
+	public final static int ONE_SECOND = 1000;
+
 	private Handler mHandler = new Handler();
 	private Runnable updateProgress;
-	
+
 	TextView timerText;
-	Button answerOneB, answerTwoB, answerThreeB, answerFourB, storeOneB, storeTwoB;
+	Button answerOneB, answerTwoB, answerThreeB, answerFourB, storeOneB,
+			storeTwoB;
 	ProgressBar enemyHealthPB, playerHealthPB, timeLeftPB;
-	
-	
-	int secondsLeft = 15*10;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        updateProgress = new Runnable() {
-        	public void run() {
-        		
-        		if(secondsLeft <= 0) {
-        			GameOver(true);
-        		} else {
-        	    	timerText.setText(Integer.toString((secondsLeft/10)+1));
-        			secondsLeft--;
-	        		timeLeftPB.setProgress(secondsLeft);
-	        		mHandler.postDelayed(this, ONE_SECOND/10);
-        		}
-        	}
-        };
-        
-        answerOneB = (Button)findViewById(R.id.answerOne);
-        answerTwoB = (Button)findViewById(R.id.answerTwo);
-        answerThreeB = (Button)findViewById(R.id.answerThree);
-        answerFourB = (Button)findViewById(R.id.answerFour);
-        
-        timerText = (TextView)findViewById(R.id.txtTimeLeft);
-        
-        enemyHealthPB = (ProgressBar)findViewById(R.id.enemyHealth);
-        playerHealthPB = (ProgressBar)findViewById(R.id.playerHealth);
-        timeLeftPB = (ProgressBar)findViewById(R.id.timeLeft);
-        
-        answerOneB.setOnClickListener(this);
-        answerTwoB.setOnClickListener(this);
-        answerThreeB.setOnClickListener(this);
-        answerFourB.setOnClickListener(this);
-        
-        
-        startTimer();
-    }
-    
-    
-    // REMEMBER TO RESET secondsLeft before calling this
-    public void startTimer() {
-    	
-    	timeLeftPB.setMax(secondsLeft);
+
+	int secondsLeft = 15 * 10;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		updateProgress = new Runnable() {
+			public void run() {
+
+				if (secondsLeft <= 0) {
+					GameOver(true);
+				} else {
+					timerText.setText(Integer.toString((secondsLeft / 10) + 1));
+					secondsLeft--;
+					timeLeftPB.setProgress(secondsLeft);
+					mHandler.postDelayed(this, ONE_SECOND / 10);
+				}
+			}
+		};
+
+		answerOneB = (Button) findViewById(R.id.answerOne);
+		answerTwoB = (Button) findViewById(R.id.answerTwo);
+		answerThreeB = (Button) findViewById(R.id.answerThree);
+		answerFourB = (Button) findViewById(R.id.answerFour);
+
+		storeOneB = (Button) findViewById(R.id.moreTime);
+		storeTwoB = (Button) findViewById(R.id.moreHP);
+
+		timerText = (TextView) findViewById(R.id.txtTimeLeft);
+
+		enemyHealthPB = (ProgressBar) findViewById(R.id.enemyHealth);
+		playerHealthPB = (ProgressBar) findViewById(R.id.playerHealth);
+		timeLeftPB = (ProgressBar) findViewById(R.id.timeLeft);
+
+		answerOneB.setOnClickListener(this);
+		answerTwoB.setOnClickListener(this);
+		answerThreeB.setOnClickListener(this);
+		answerFourB.setOnClickListener(this);
+
+		storeOneB.setOnClickListener(this);
+		storeTwoB.setOnClickListener(this);
+
+		startTimer();
+	}
+
+	// REMEMBER TO RESET secondsLeft before calling this
+	public void startTimer() {
+
+		timeLeftPB.setMax(secondsLeft);
 		timeLeftPB.setProgress(secondsLeft);
-    	mHandler.removeCallbacks(updateProgress);
-    	mHandler.postDelayed(updateProgress, ONE_SECOND/10);
-    }
-    
-    public void onClick(View src) {
-    	
-    }
-    
-    public void GameOver(boolean timeExpired) {
-    	secondsLeft = 150;
-    	startTimer();
-    }
-    
+		mHandler.removeCallbacks(updateProgress);
+		mHandler.postDelayed(updateProgress, ONE_SECOND / 10);
+	}
+
+	public void onClick(View src) {
+		switch (src.getId()) {
+		case R.id.answerOne:
+			choseAnswer(0);
+			break;
+		case R.id.answerTwo:
+			choseAnswer(1);
+			break;
+		case R.id.answerThree:
+			choseAnswer(2);
+			break;
+		case R.id.answerFour:
+			choseAnswer(3);
+			break;
+		case R.id.moreHP:
+			purchasedHP();
+			break;
+		case R.id.moreTime:
+			purchasedTime();
+			break;
+		}
+	}
+	
+	public void choseAnswer(int answerChosen) {
+		
+	}
+	
+	public void purchasedHP() {
+		
+	}
+	
+	public void purchasedTime() {
+		
+	}
+
+	public void GameOver(boolean timeExpired) {
+		secondsLeft = 150;
+		startTimer();
+	}
+
 }
